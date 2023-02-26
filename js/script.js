418289038f814a0351647cc19ce6d976a68d6695
Naimur Reza <naimur.rezaa@gmail.com>
2023-02-26 20:52:32 +0600

    New ui

--- js/script.js
+++ js/script.js
@@ -10,19 +10,22 @@ const displayPhone = (phones) => {
   container.innerHTML = "";
   phones = phones.slice(0, 10);
   //get no phone msg
-  const noFound = document.getElementById('no-phone-msg')
-  if(phones.length == 0){
-    noFound.classList.remove('hidden')
+  const noFound = document.getElementById("no-phone-msg");
+  if (phones.length == 0) {
+    noFound.classList.remove("hidden");
   }
-  
+  else{
+    noFound.classList.add("hidden");
+  }
+
   phones.forEach((phone) => {
     const newElement = document.createElement("div");
     newElement.innerHTML = `
-        <div class="shadow-xl rounded-lg w-56 p-5 space-y-4">
+        <div class="shadow-xl rounded-lg w-56 p-5 space-y-4 text-white">
         <img class="rounded-2xl w-fit mx-auto" src="${phone.image}" alt="">
         <h1><span class="font-semibold">Brand</span> : ${phone.brand} </h1>
         <h1><span class="font-semibold">Name</span> : ${phone.phone_name}</h1>
-        <button class="py-2 px-4 bg-teal-400 rounded-lg hover:bg-teal-300 transition-all font-semibold text-white">Buy Now</button>
+        <button class="py-2 px-4 bg-[#FD297A] rounded-lg hover:bg-teal-300 transition-all font-semibold text-white">Buy Now</button>
     </div>
     `;
     container.appendChild(newElement);
@@ -34,4 +37,4 @@ document.getElementById("search-btn").addEventListener("click", function () {
   loadPhone(value);
 });
 
-loadPhone('a');
+loadPhone("a");
